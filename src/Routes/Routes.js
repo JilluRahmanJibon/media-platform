f211bb8d9fbb07b983bafc09038a494a79f7359e
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2022-12-27 09:55:20 +0600

    all pages and route created

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,8 @@
 import ErrorPage from "../ErrorPage/ErrorPage";
 import Root from "../Layout/Root";
+import Faq from "../Pages/FAQ/FAQ";
+import PrivacyPolicy from "../Pages/PrivacyPolicy/PrivacyPolicy";
+import TermsConditions from "../Pages/Terms&Conditions/Terms&Conditions";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -11,7 +14,9 @@ const router = createBrowserRouter([
             { path: '/', element: '' },
             { path: '/', element: '' },
             { path: '/', element: '' },
-            { path: '/', element: '' },
+            { path: '/Faq', element: <Faq/>},
+            { path: '/PrivacyPolicy', element: <PrivacyPolicy/> },
+            { path: '/TermsConditions', element: <TermsConditions/> },
     ] },
      
 ])
