f699c27e572a2a36bad518dde36cfc40fc1de56c
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2022-12-27 08:52:48 +0600

    Navbar and footer added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,7 +1,19 @@
+import ErrorPage from "../ErrorPage/ErrorPage";
+import Root from "../Layout/Root";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
-    { path:'/',element:''}
+    {
+        errorElement:<ErrorPage/>,
+        path: '/', element: <Root />, children: [
+        
+            { path: '/', element: '' },
+            { path: '/', element: '' },
+            { path: '/', element: '' },
+            { path: '/', element: '' },
+    ] },
+     
 ])
 
 export default router
---
+++ src/Shared/Loader/SmallLoader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SmallLoader = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SmallLoader;
